274680fa932de00d2ab789f5489d5ab5be78b517
muhammadganny <mohammadganny@gmail.com>
2023-08-02 10:49:09 +0500

    navbar

---
+++ src/components/Signin.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Logo from "../logo.png";
+
+const Signin = () => {
+
+    return(
+      <div>
+        <div className="navbar">
+          <div className="logo">
+            <img className="Netflix-logo" src={Logo} alt="" />
+          </div>
+        </div>
+        
+      </div>
+        
+
+    )
+    
+}
+export default Signin;
